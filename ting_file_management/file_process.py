fa520939b45192f2fdfdcc81546652100c272552
matheusnff85 <math.marinho154@gmail.com>
2023-02-03 16:47:13 -0300

    feat: requisito 3 função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
+
+
+def process(file_path, queue):
+    files = [item["nome_do_arquivo"] for item in queue.items]
+
+    if file_path not in files:
+        file = txt_importer(file_path)
+        dict_data = {
+            "nome_do_arquivo": file_path,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file,
+        }
+        queue.enqueue(dict_data)
+        print(dict_data, file=sys.stdout)
 
 
 def remove(instance):
