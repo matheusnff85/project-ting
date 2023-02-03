e34438b5ef4eb519be363fd171e7c7faeb669dae
matheusnff85 <math.marinho154@gmail.com>
2023-02-03 16:12:33 -0300

    feat: requisito 2 file management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+        return []
+
+    try:
+        with open(path_file, "r") as file:
+            return file.read().split("\n")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return []
